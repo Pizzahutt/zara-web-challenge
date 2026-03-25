bd6de4be2903058f1e4e24d9d116b85d2c9ea289
Pizzahutt <ferrenguix@gmail.com>
2026-03-25 21:31:22 +0100

    chore: move API key to env variable, add Playwright artifacts to gitignore

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = 'https://prueba-tecnica-api-tienda-moviles.onrender.com';
-export const API_KEY = '87909682e6cd74208f41a6ef39fe4191';
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY ?? '';
 
 export const PRODUCTS_HOME_LIMIT = 20;
 
